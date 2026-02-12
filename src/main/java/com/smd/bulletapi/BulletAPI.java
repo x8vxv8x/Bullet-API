1a0fdaf82dc121fb5e08462fbbba8b60344ef047
smd <88483238+x8vxv8x@users.noreply.github.com>
2026-02-13 02:13:04 +0800

    基础框架

--- src/main/java/com/smd/bulletapi/BulletAPI.java
+++ src/main/java/com/smd/bulletapi/BulletAPI.java
@@ -1,13 +1,13 @@
-package com.example.modid;
+package com.smd.bulletapi;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class BulletAPI {
+
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
